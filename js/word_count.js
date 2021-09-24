b8016c9c914c3d0c53f9f1c7fc4de296da5bace2
Ezequiel Morais <ezequielmorais4002@gmail.com>
2021-09-24 10:10:30 -0300

    Word count implementation

--- js/word_count.js
+++ js/word_count.js
@@ -4,24 +4,30 @@ $(document).ready(()=>{
 	let palavras = 0
 	let letras= 0
 	let paragrafos = 0
+	let spacenum = 0
 
 	$("#word-num").html(palavras)
 	$("#letter-num").html(letras)
 	$("#paragraph-num").html(paragrafos)
 
-	$("textarea").keydown(()=>{
+	$("textarea").keyup(()=>{
 		texto = $("textarea").val()
-		palavras = texto.split(" ").length
-
-		letras = texto.split("").length + 1
-
-		paragrafos = texto.split("\n").length
-
-
-		if (texto == '') {
+		if (texto == "") {
 			palavras = 0
 			letras = 0
 			paragrafos = 0
+		}
+		else{
+			/* LÓGICA CONTAGEM DE LETRAS */
+			if(texto.split("")[texto.split("").length - 1] != " "){
+				letras = 0
+				texto.split("").map((e)=>{
+					if (e != " ") {
+						letras ++
+					}
+				})
+			}
+			/* FIM LÓGICA CONTAGEM DE LETRAS */
 		}
 
 		$("#word-num").html(palavras)
