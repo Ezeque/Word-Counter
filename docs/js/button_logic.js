c5f5c0135365095c2e9c7f4c9d9139c0e9a33b44
Ezequiel Morais <ezequielmorais4002@gmail.com>
2021-09-29 12:30:41 -0300

    improved capitalize logic *fixed*

--- docs/js/button_logic.js
+++ docs/js/button_logic.js
@@ -2,6 +2,8 @@ $(document).ready(()=>{
 	let c_text
 	let aux
 	let check_last = false
+
+	/* LÓGICA CAPITALIZE */
 	$("#capitalize").click(()=>{
 		if($("textarea").val() != aux){
 			$("textarea").val(v.trimRight($("textarea").val(), "."))
@@ -16,6 +18,17 @@ $(document).ready(()=>{
 				e = v.trim(e)
 				e = v.capitalize(e)
 				$("textarea").val($("textarea").val() + e + ". ")
+				if($("textarea").val()[$("textarea").val().indexOf(e) + 1] == "\n"){
+					$("textarea").val($("textarea").val() + e + ". ")
+				}
+			})
+
+			aux = $("textarea").val().split("\n")
+			$("textarea").val("")
+			aux.map((e)=>{
+				e = v.trim(e)
+				e = v.capitalize(e)
+				$("textarea").val($("textarea").val() + e + "\n")
 			})
 			console.log($("textarea").val().split("")[$("textarea").val().split("").length - 1])
 			$("textarea").val(v.trim($("textarea").val()))
